a6b043f36ad21e93d035666322027827c00a8dcb
Tyler Allen <mynameistylerallen@gmail.com>
2017-04-28 15:06:32 -0700

    debug if statement and change character at position 0 of each input selector value to lowercase to get if statment to perform properly.

--- js/scripts.js
+++ js/scripts.js
@@ -9,31 +9,47 @@
 //Front-End
 $(function() {
 
-  $("#formQuestionOne").submit(function(event) {
+  $("#formSurvey").submit(function(event) {
     event.preventDefault();
     var question1Answer = $("input:radio[name=question1]:checked").val();
-    alert(question1Answer);
     var question2Answer = $("input:radio[name=question2]:checked").val();
-    alert(question2Answer);
     var question3Answer = $("input:radio[name=question3]:checked").val();
-    alert(question3Answer);
     var question4Answer = $("input:radio[name=question4]:checked").val();
-    alert(question4Answer);
     var question5Answer = $("input:radio[name=question5]:checked").val();
-    alert(question5Answer);
+
+
     if (question1Answer === "frontEnd") {
       $(".trackSuggestions").hide();
-      $("formQuestionTwo").show();
       $("#cssDesign").show();
     }
+    else if (question2Answer === "yes") {
+      if (question3Answer == "small") {
+        $(".trackSuggestions").hide();
+        $("#rubyRails").show();
+      }
+      else if (question3Answer === "large") {
+        $(".trackSuggestions").hide();
+        $("#cSharpNet").show();
+      }
+    }
+
+    else if (question2Answer === "no" && question4Answer === "mobile"){
+      $(".trackSuggestions").hide();
+      $("#javaAndroid").show();
+    }
+    else if (question2Answer === "no" && question4Answer === "desktop" && question5Answer === "management") {
+      $(".trackSuggestions").hide();
+      $("#phpDrupal").show();
+    }
+    else if (question2Answer === "no" && question4Answer === "desktop" && question5Answer === "interactive"){
+      $(".trackSuggestions").hide();
+      $("#phpReact").show();
+    }
     else {
-      //   if (question2Answer === "frontEnd") {
-      //     $(".trackSuggestions").hide();
-      //     $("#cssDesign").show();
-        // }
-
-
-
+      alert(question2Answer);
+      alert(question3Answer);
+      alert(question4Answer);
+      alert(question5Answer);
     }
   });
 });
