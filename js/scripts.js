1539eef9b119184d857f7e552e3f2ff35261c179
Tyler Allen <mynameistylerallen@gmail.com>
2017-04-28 14:12:38 -0700

    consolidate form selectors into one, delete form ids from css.styles.css that were consolidated, and create variables for every question.

--- js/scripts.js
+++ js/scripts.js
@@ -8,24 +8,29 @@
 
 //Front-End
 $(function() {
-  alert("hello");
+
   $("#formQuestionOne").submit(function(event) {
     event.preventDefault();
     var question1Answer = $("input:radio[name=question1]:checked").val();
     alert(question1Answer);
+    var question2Answer = $("input:radio[name=question2]:checked").val();
+    alert(question2Answer);
+    var question3Answer = $("input:radio[name=question3]:checked").val();
+    alert(question3Answer);
+    var question4Answer = $("input:radio[name=question4]:checked").val();
+    alert(question4Answer);
+    var question5Answer = $("input:radio[name=question5]:checked").val();
+    alert(question5Answer);
     if (question1Answer === "frontEnd") {
       $(".trackSuggestions").hide();
+      $("formQuestionTwo").show();
       $("#cssDesign").show();
     }
     else {
-      $("#formQuestionTwo").show();
-      $("#formQuestionTwo").submit(function(event) {
-        event.preventDefault();
-        var question2Answer = $("input:radio[name=question2]:checked").val();
-        if (question2Answer === "frontEnd") {
-          $(".trackSuggestions").hide();
-          $("#cssDesign").show();
-        }
+      //   if (question2Answer === "frontEnd") {
+      //     $(".trackSuggestions").hide();
+      //     $("#cssDesign").show();
+        // }
 
 
 
