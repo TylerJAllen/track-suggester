c2ee5d29ae55e1f18250d62c47d30d46156bd931
Tyler Allen <mynameistylerallen@gmail.com>
2017-04-28 13:41:01 -0700

    add div.trackSuggestions for #cssDesign, #rubyRails, #cSharpNet, #javaAndroid, #phpReact, and #phpDrupal and set all to display:none. Add #formQuestionThree, #formQuestionFour, and #formQuestionFive to css and set to display:none as well.

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,27 @@
 
 //Front-End
 $(function() {
+  alert("hello");
   $("#formQuestionOne").submit(function(event) {
     event.preventDefault();
     var question1Answer = $("input:radio[name=question1]:checked").val();
+    alert(question1Answer);
     if (question1Answer === "frontEnd") {
       $(".trackSuggestions").hide();
       $("#cssDesign").show();
     }
     else {
+      $("#formQuestionTwo").show();
+      $("#formQuestionTwo").submit(function(event) {
+        event.preventDefault();
+        var question2Answer = $("input:radio[name=question2]:checked").val();
+        if (question2Answer === "frontEnd") {
+          $(".trackSuggestions").hide();
+          $("#cssDesign").show();
+        }
+
+
+
     }
-
-
-
   });
 });
